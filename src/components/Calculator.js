8f53ced1c9c48e3faccd19669c351310ba8fd926
costikluciu2020 <costikluciu2020@gmail.com>
2022-11-14 22:25:45 +0000

    Installed SASS compiler and created a rough grid layout

---
+++ src/components/Calculator.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+class Calculator extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		return (
+		<div className="calculator">
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		<div>bt</div>
+		</div>
+		);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Calculator;
+
+
+
+
+
