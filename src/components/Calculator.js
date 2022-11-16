04ce90a9d205e20d874968c5c417c8382d430d70
costikluciu2020 <costikluciu2020@gmail.com>
2022-11-16 04:27:23 +0000

    Created a rough layout for my React calculator using grid layout in SASS

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 class Calculator extends React.Component{
 	constructor(props){
 		super(props);
@@ -9,26 +8,24 @@ class Calculator extends React.Component{
 	render(){
 		return (
 		<div className="calculator">
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
-		<div>bt</div>
+	        <div className="display">0000</div>
+            <div id="clear">AC</div>
+            <div id="divide">/</div>
+            <div id="multiply">*</div>
+            <div id="substract">-</div>
+            <div id="add">+</div>
+            <div id="equal">=</div>
+            <div id="seven">7</div>
+            <div id="eight">8</div>
+            <div id="nine">9</div>
+            <div id="four">4</div>
+            <div id="five">5</div>
+            <div id="six">6</div>
+            <div id="one">1</div>
+            <div id="two">2</div>
+            <div id="three">3</div>
+            <div id="zero">0</div>
+            <div id="dot">.</div>
 		</div>
 		);
 	}
